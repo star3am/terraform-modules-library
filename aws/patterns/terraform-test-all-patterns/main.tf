89a9971ef66ccbd89b513634e6f8518d121d8aa5
Riaan Nolan <riaan.nolan@gmail.com>
2024-02-13 09:48:22 +0000

    small bux fix

---
+++ aws/patterns/terraform-test-all-patterns/main.tf
@@ -0,0 +1,21 @@
+# This is here to deploy all patterns together to TF Cloud Workspace
+
+module "s3_bucket_standard" {
+  source = "../s3-bucket-standard"
+}
+
+module "s3_bucket_object_lock" {
+  source = "../s3-bucket-object-lock"
+}
+
+module "s3_bucket_glacier_transition" {
+  source = "../s3-bucket-glacier-transition"
+}
+
+module "rds_postgresql" {
+  source = "../rds-postgresql"
+}
+
+module "rds_mssql" {
+  source = "../rds-mssql"
+}
