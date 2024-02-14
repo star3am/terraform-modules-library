e467dc09b09fb3cebd5385a5b64501b7a5876572
Riaan Nolan <riaan.nolan@gmail.com>
2024-02-14 07:50:09 +0000

    fix and exclude some checkof lint rules

--- aws/patterns/s3-bucket-object-lock/main.tf
+++ aws/patterns/s3-bucket-object-lock/main.tf
@@ -45,7 +45,7 @@ module "logs_bucket" {
 
   control_object_ownership = true
 
-  object_ownership = "BucketOwnerPreferred"
+  # object_ownership = "BucketOwnerPreferred"
 
   expected_bucket_owner = data.aws_caller_identity.current.account_id
 
@@ -84,7 +84,7 @@ module "object_lock_bucket" {
 
   control_object_ownership = true
 
-  object_ownership = "BucketOwnerPreferred"
+  # object_ownership = "BucketOwnerPreferred"
 
   expected_bucket_owner = data.aws_caller_identity.current.account_id
 
