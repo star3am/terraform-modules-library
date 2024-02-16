72a59f2e7356a7509136dd6ed95ff44f5528c923
Riaan Nolan <riaan.nolan@gmail.com>
2024-02-16 01:24:06 +0000

    ignore checkov CKV_TF_1 and skip tflint terraform_module_pinned_source

--- aws/patterns/terraform-test-all-patterns/docs/README.md
+++ aws/patterns/terraform-test-all-patterns/docs/README.md
@@ -8,33 +8,33 @@ The following Modules are called:
 
 ### <a name="module_rds_mssql"></a> [rds_mssql](#module_rds_mssql)
 
-Source: ../rds-mssql
+Source: github.com/star3am/terraform-modules-library//aws/patterns/rds-mssql
 
-Version:
+Version: main
 
 ### <a name="module_rds_postgresql"></a> [rds_postgresql](#module_rds_postgresql)
 
-Source: ../rds-postgresql
+Source: github.com/star3am/terraform-modules-library//aws/patterns/rds-postgresql
 
-Version:
+Version: main
 
 ### <a name="module_s3_bucket_glacier_transition"></a> [s3_bucket_glacier_transition](#module_s3_bucket_glacier_transition)
 
-Source: ../s3-bucket-glacier-transition
+Source: github.com/star3am/terraform-modules-library//aws/patterns/s3-bucket-glacier-transition
 
-Version:
+Version: main
 
 ### <a name="module_s3_bucket_object_lock"></a> [s3_bucket_object_lock](#module_s3_bucket_object_lock)
 
-Source: ../s3-bucket-object-lock
+Source: github.com/star3am/terraform-modules-library//aws/patterns/s3-bucket-object-lock
 
-Version:
+Version: main
 
 ### <a name="module_s3_bucket_standard"></a> [s3_bucket_standard](#module_s3_bucket_standard)
 
-Source: ../s3-bucket-standard
+Source: github.com/star3am/terraform-modules-library//aws/patterns/s3-bucket-standard
 
-Version:
+Version: main
 
 ## Resources
 
--- aws/patterns/terraform-test-all-patterns/main.tf
+++ aws/patterns/terraform-test-all-patterns/main.tf
@@ -5,21 +5,31 @@ terraform {
 }
 
 module "s3_bucket_standard" {
+  #checkov:skip=CKV_TF_1: Ensure Terraform module sources use a commit hash
+  #tflint-ignore: terraform_module_pinned_source
   source = "github.com/star3am/terraform-modules-library//aws/patterns/s3-bucket-standard?ref=main"
 }
 
 module "s3_bucket_object_lock" {
+  #checkov:skip=CKV_TF_1: Ensure Terraform module sources use a commit hash
+  #tflint-ignore: terraform_module_pinned_source
   source = "github.com/star3am/terraform-modules-library//aws/patterns/s3-bucket-object-lock?ref=main"
 }
 
 module "s3_bucket_glacier_transition" {
+  #checkov:skip=CKV_TF_1: Ensure Terraform module sources use a commit hash
+  #tflint-ignore: terraform_module_pinned_source
   source = "github.com/star3am/terraform-modules-library//aws/patterns/s3-bucket-glacier-transition?ref=main"
 }
 
 module "rds_postgresql" {
+  #checkov:skip=CKV_TF_1: Ensure Terraform module sources use a commit hash
+  #tflint-ignore: terraform_module_pinned_source
   source = "github.com/star3am/terraform-modules-library//aws/patterns/rds-postgresql?ref=main"
 }
 
 module "rds_mssql" {
+  #checkov:skip=CKV_TF_1: Ensure Terraform module sources use a commit hash
+  #tflint-ignore: terraform_module_pinned_source
   source = "github.com/star3am/terraform-modules-library//aws/patterns/rds-mssql?ref=main"
 }
