521bbb853e2ec701eea317e263f7f67cb26aa473
Riaan Nolan <riaan.nolan@gmail.com>
2024-07-04 22:01:43 +0000

    adding terraform-aws-single-ec2-vm pattern

--- aws/patterns/terraform-aws-single-ec2-vm/main.tf
+++ aws/patterns/terraform-aws-single-ec2-vm/main.tf
@@ -1,3 +1,8 @@
-terraform {
-  required_version = "~> 1.0"
+module "terraform_aws_single_ec2_vm" {
+  #checkov:skip=CKV_TF_1: Ensure Terraform module sources use a commit hash
+  source  = "app.terraform.io/nolan/single-ec2-vm/aws"
+  version = "0.0.1"
+
+  name    = var.name
+  os_type = var.os_type
 }
